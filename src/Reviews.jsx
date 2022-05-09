6514a7aa6e1b75cf46215c72a77d99ee31707b75
hdpyo <hudongpyo.dev@gmail.com>
2022-05-09 15:35:54 +0900

    Refactor: <Reivews />

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -4,14 +4,27 @@ export default function Reviews({ reviews }) {
   }
 
   return (
-    <>
+    <ul>
       {reviews.map((review) => (
-        <div key={review.id}>
-          {review.name}
-          |
-          {review.description}
-        </div>
+        <li key={review.id}>
+          <div>
+            작성자 :
+            {' '}
+            {review.name}
+          </div>
+          <div>
+            점수 :
+            {' '}
+            {review.score}
+            점
+          </div>
+          <div>
+            내용 :
+            {' '}
+            {review.description}
+          </div>
+        </li>
       ))}
-    </>
+    </ul>
   );
 }
